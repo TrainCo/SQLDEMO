9458dcf734ca6876ee34545006fefdff8289a0e8
will davis <v-wildavis@microsoft.com>
2026-05-28 09:45:59 -0400

    i fixed it

--- PostDeployment/Script.PostDeployment.sql
+++ PostDeployment/Script.PostDeployment.sql
@@ -4,7 +4,7 @@ PRINT 'Seeding demo data...';
 IF NOT EXISTS (SELECT 1 FROM dbo.Product)
 BEGIN
     INSERT INTO dbo.Product (ProductName)
-    VALUES 
+    VALUES --a comment
         ('Laptop'),
         ('Tablet'),
         ('Phone');
